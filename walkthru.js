c891b6386ac3f3fb5d0bc93f27704c894379f065
Matt Haggard <haggardii@gmail.com>
2013-05-15 15:17:06 -0600

    Cleaner syntax for setting up steps

--- walkthru.js
+++ walkthru.js
@@ -75,55 +75,124 @@ $.fn.walkthru = function(action, options) {
 };
 
 var walkthru = window.walkthru || {};
-walkthru.doSequence = function(options) {
-  var settings = $.extend({
-    "units": 1000,
-    "delayBefore": 0,
-    "delayAfter": 0,
-    "actions": []
-  }, options);
 
-  var current_time = 0;
-  settings.actions.forEach(function(action) {
-    var delayBefore = action.delayBefore || settings.delayBefore;
-    var delayAfter = action.delayAfter || settings.delayAfter;
-    var delay = current_time + delayBefore;
-    setTimeout(action.action, delay * settings.units);
-    current_time = delay + delayAfter;
-  })
-  return current_time;
+
+walkthru.wait = function(ms) {
+  return function() {
+    var x = $.Deferred();
+    setTimeout(function() {
+      x.resolve(ms);
+    }, ms);
+    return x.promise();
+  }
 }
 
-var remaining_steps = [];
-walkthru.walkthru = function(options) {
+
+function methodize(obj, func) {
+  // because I don't know what I'm doing :)
+  return function() {
+    return func.apply(obj, arguments);
+  };
+};
+
+
+
+var WalkThru = function() {
   this._remaining_steps = [];
   this._current_step = null;
+  this._reaction = new Reaction();
 
-  this.next = function() {
-    var delay = 0;
-    if (this._current_step && this._current_step.cleanup) {
-      delay = this._current_step.cleanup();
+  this.addStep = methodize(this, function(func) {
+    var step = new Step(null, func);
+    this._reaction.add(function() {
+      return step.run();
+    });
+  });
+
+  this.start = methodize(this, function() {
+    return this._reaction.start();
+  });
+};
+
+
+
+var Step = function(walkthru, func) {
+  this._walkthru = walkthru;
+  this._func = func;
+
+  this._d = new $.Deferred();
+  
+  this.setup = new Reaction();
+  this.postSuccess = new Reaction();
+  this.teardown = new Reaction();
+  this.postTeardown = new Reaction();
+
+  this.run = methodize(this, function() {
+    this._func(this);
+    this.setup.start();
+    return this._d.promise();
+  });
+
+  this.succeed = methodize(this, function() {
+    this.setup.stop();
+    this.postSuccess.start()
+      .then(this._doTeardown);
+  });
+
+  this._doTeardown = methodize(this, function() {
+    return this.teardown.start()
+      .then(this.postTeardown.start)
+      .then(function() {
+        this._d.resolve(null);
+      }.bind(this));
+  });
+
+  // Bind an event listener only during this step (remove it on finish).
+  this.bind = methodize(this, function(elem, what, handler) {
+    $(elem).bind(what, handler);
+    this.teardown.add(function() {
+      $(elem).unbind(what, handler);
+    });
+  });
+
+};
+
+
+var Reaction = function() {
+  this._running = false;
+  this._queue = [];
+
+  this.start = methodize(this, function() {
+    this._running = true;
+    return $.when(this._next());
+  });
+
+  this.stop = methodize(this, function() {
+    this._running = false;
+  });
+
+  this._next = methodize(this, function() {
+    if (!this._running) {
+      return;
     }
-    if (delay === null || delay === undefined) {
-      delay = 0;
+    if (this._queue.length) {
+      var func = this._queue.shift();
+      return $.when(func()).then(this._next.bind(this));
     }
+  });
 
-    this._current_step = this._remaining_steps.shift();
-    if (this._current_step) {
-      setTimeout(this._current_step.action, delay);
-    }    
-  }.bind(this);
-
-  this.addSteps = function(steps) {
-    this._remaining_steps = steps;
-  }.bind(this);
-
-  this.start = function() {
-    this.next();
-  }.bind(this);
-
-  return this;
+  this.add = methodize(this, function(func) {
+    this._queue.push(func);
+    if (this._running && this._queue.length == 1) {
+      this._next();
+    }
+    return this;
+  });
 }
+
+
+walkthru.WalkThru = WalkThru;
+
 
 window.walkthru = walkthru;
 
